9f9873f4681fc5d58e2ca68592fef19e74502f52
aynurcelebi <aynurcelebi8947@gmail.com>
2023-02-01 00:35:23 +0300

    .

--- src/concreate/strategy/SuvStrategyImpl.java
+++ src/concreate/strategy/SuvStrategyImpl.java
@@ -1,11 +1,24 @@
 package concreate.strategy;
 
-import concreate.Car;
-import concreate.Customer;
+import concreate.*;
 
 public class SuvStrategyImpl implements IRentStrategy {
     @Override
     public void rentACar(Car car, Customer customer) {
+        double suvMounthlyParam = 1.1;
+
+
+        if (customer instanceof Person){
+            System.out.println("You are not allowed for this operation!!");
+        }
+        else {
+           if (RentType.MONTHLY.equals(car.getRentInfo().getRentType())){
+
+               RentInfo rentInfo  = new RentInfo();
+               double mounthlyPrice = suvMounthlyParam * car.getRentInfo().getMonthlyRentPrice();
+               rentInfo.setTotalPrice(mounthlyPrice);
+           }
+        }
         System.out.println("SUV Kiralama");
     }
 
